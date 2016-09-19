89ff9eb7471c56281d2ea804f49a79c3ddf14dbc
HusenudinN <Husenudin.nurdiansyah@gmail.com>
2016-09-19 09:59:29 +0700

    Input source code website dvd installer linux

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/LinuxInstall.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/LinuxInstall.com/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/LinuxInstall.com/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/LinuxInstall.com/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/LinuxInstall.com/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/LinuxInstall.com/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/LinuxInstall.com/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/LinuxInstall.com/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/LinuxInstall.com/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/LinuxInstall.com/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/LinuxInstall.com/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/LinuxInstall.com/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/LinuxInstall.com/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'linux_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
